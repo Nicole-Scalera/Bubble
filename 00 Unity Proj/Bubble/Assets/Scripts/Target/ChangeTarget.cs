bf57f0b8abbde783248cc33548fa836d20c88782
Annadaliah <112446326+Annadaliah@users.noreply.github.com>
2025-02-08 15:48:45 -0500

    Added Comments
    - Added as many comments as I could explaining what I did and why I did it

--- 00 Unity Proj/Bubble/Assets/Scripts/Target/ChangeTarget.cs
+++ 00 Unity Proj/Bubble/Assets/Scripts/Target/ChangeTarget.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ChangeTarget : MonoBehaviour
 {
-    public GameObject nextTargetPoint;
-    public GameObject followingTargetPoint;
+    /*This script was made in order to give the Targets that the player passes over so there is an exchange of information to the PlayerRespawn script attached the player
+    */
+    public GameObject nextTargetPoint; //Stores the very next save point the player has to reach within the Target Object
+    public GameObject followingTargetPoint; //Stores the save point after the last one so that there is an easier transfer of information without having to write long extensive code
 
 }
