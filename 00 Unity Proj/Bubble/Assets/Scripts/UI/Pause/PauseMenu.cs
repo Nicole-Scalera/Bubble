e44122136a2ca06fc4f69991b795561205a215c4
Annadaliah <112446326+Annadaliah@users.noreply.github.com>
2025-02-09 12:31:38 -0500

    Menus and Other Changes
    - Created the PauseMenu with buttons and scripts needed
    - Created the GameOverMenu with buttons and scripts needed
    - Added some images into the game
    - Things are not done as i am trying to get the PlayerLives variable usable and make the variable open the GameOverMenu when 0
    - not done though

--- 00 Unity Proj/Bubble/Assets/Scripts/UI/Pause/PauseMenu.cs
+++ 00 Unity Proj/Bubble/Assets/Scripts/UI/Pause/PauseMenu.cs
@@ -1,36 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu = null;
     public bool isPaused;
-    public Button pauseButton;
+    public GameObject playerObject;
+    private UnityEngine.Vector2 hiddenMenuPosition;
+    private UnityEngine.Vector2 visibleMenuPosition;
+    public GameObject menuStartPosition;
+    public Button resumeButton;
+    public Button restartButton;
+    public Button quitButton;
+    void Start()
+    {
+        resumeButton = GetComponent<Button>();
+        restartButton = GetComponent<Button>();
+        quitButton = GetComponent<Button>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
         }
-
-        /*if (Input.()) == true)
-        {
-            ResumeGame();
-        }*/
     }
     void PauseGame()
     {
         isPaused = true;
         Time.timeScale = 0;
         pauseMenu.SetActive(isPaused);
+        MoveMenuInView();
     }
-    void ResumeGame()
+    public void ResumeGame()
     {
         isPaused = false;
         Time.timeScale = 1;
         pauseMenu.SetActive(isPaused);
+        MoveMenuOutOfView();
+    }
+    void MoveMenuInView()
+    {
+        visibleMenuPosition = pauseMenu.transform.position;
+        playerObject.transform.position = pauseMenu.transform.position;   
+    }
+    void MoveMenuOutOfView()
+    {
+        hiddenMenuPosition = menuStartPosition.transform.position;
+    }
+    public void RestartGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("AltLevel");
+    }
+    public void QuitGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
     }
 }
