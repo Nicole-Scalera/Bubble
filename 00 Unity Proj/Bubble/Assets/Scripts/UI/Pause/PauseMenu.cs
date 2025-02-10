dfd8d263670426ee413fead0cefef84950bcffe3
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-02-10 15:12:04 -0500

    Fixed Pause & Game Over Menus
     Overview
    - Fixed the UI screen resolution of Pause & Game Over pop-ups.
    - Implemented a few singleton instances for global referencing.
    - Created new files.
    
    In-depth Details:
    - I will break down the edits below.
    
    A. General
    - Created VirtCamInfo.cs to allow the Virtual Camera (fce996c) to persist across scenes.
    - Added this same "persist across scenes" feature to CameraInfo.cs.
    
    B. UI Updated
    - Updated the buttons spritesheet to feature the new buttons @paulmait1 added (8843d53).
    - Implemented the new Resume and Exit buttons into the Pause and Game Over menus.
         - This replaced the game objects @Annadaliah added named "SubsituteWith[name]Later" (f0d52c6).
    - Updated the PauseMenu and GameOverMenu game objects to fit to the screen view.
         - Items are now activated and deactivated from view, rather than moving them into view (f0d52c6).
         - Child objects taken out of "Image" and "Button" game object sorters.
              - This is because it disables quick snapping in the UI for some reason.
         - All items are appended to a list named "contents" inside of the scripts.
    
    C. Constant Speed
    - Updated PlayerMovement.cs to allow a constant speed of the player rather than having to build up to terminal velocity.
         - See #35 for more information.
    
    C. Singletons
    - Added a few singleton global references for the following:
         - Player.cs
              - New file for referencing the player object and the input controls.
              - Might merge this into PlayerInfo.cs later on.
         - PlayerControls.cs
              - This reference is initialized in Player.cs because PlayerControls.cs is auto-generated.
         - SceneChanger.cs
              - Needed for dynamic scene reference in PauseMenu.cs and GameOver.cs.
    
    Co-Authored-By: Annadaliah <112446326+Annadaliah@users.noreply.github.com>
    Co-Authored-By: Paul M <196402474+paulmait1@users.noreply.github.com>

--- 00 Unity Proj/Bubble/Assets/Scripts/UI/Pause/PauseMenu.cs
+++ 00 Unity Proj/Bubble/Assets/Scripts/UI/Pause/PauseMenu.cs
@@ -1,75 +1,130 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem.Android;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour, PlayerControls.IGameControlsActions
 {
-    public GameObject pauseMenu;
-    public bool isPaused;
-    public GameObject playerObject;
-    public Button resumeButton;
-    public Button restartButton;
-    public Button quitButton;
-    public GameObject buttons;
-    public GameObject images;
-    private UnityEngine.Vector2 visibleMenuPosition;
-    private bool canPause;
+    private string currentScene;
+    
+    // Booleans
+    private bool canPause = true; // Boolean to check if game can be paused
+    private bool isPaused = false; // Boolean to check if game is paused
+
+    // List to hold UI contents
+    [SerializeField] private List<GameObject> contents;
+    
+    // UI GameObjects
+    [SerializeField] private GameObject pauseMenu; // The Pause Menu GameObject
+    [SerializeField] private GameObject resumeButton; // Resume button
+    [SerializeField] private GameObject pauseRetryButton; // Retry button
+    [SerializeField] private GameObject pauseQuitButton; // Quit button
+    [SerializeField] private GameObject pauseBackground;
+
+    private PlayerControls playerControls; // PlayerControls.cs
+    
     void Awake()
     {
-        canPause = true;
-        buttons.SetActive(false);
-        images.SetActive(false);
-        pauseMenu.transform.position = playerObject.transform.position;
-        resumeButton = GetComponent<Button>();
-        restartButton = GetComponent<Button>();
-        quitButton = GetComponent<Button>();
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) & canPause == true)
+        // Get the current scene for Restart()
+        currentScene = SceneChanger.Instance.GetCurrentSceneName();
+        
+        // Initialize PlayerControls
+        playerControls = new PlayerControls();
+        playerControls.GameControls.SetCallbacks(this); // Set this class as listener
+        playerControls.GameControls.Enable();
+
+        // Find UI elements
+        pauseMenu = GameObject.Find("PauseMenu");
+        pauseBackground = GameObject.Find("PauseBackground");
+        resumeButton = GameObject.Find("ResumeButton");
+        pauseRetryButton = GameObject.Find("pauseRetryButton");
+        pauseQuitButton = GameObject.Find("pauseQuitButton");
+
+        //Create a list to store buttons
+        contents = new List<GameObject>
         {
-            PauseGame();
+            pauseMenu,
+            pauseBackground,
+            resumeButton,
+            pauseQuitButton,
+            pauseRetryButton
+        };
+        
+        // Hide UI elements
+        ToggleMenu(false);
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    
+    // Toggle Menu
+    private void ToggleMenu(bool isActive)
+    {
+        foreach (GameObject item in contents)
+        {
+            item.SetActive(isActive);
         }
     }
+
+    // Using Input System (PlayerControls.cs) to pause the game
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        // If NOT paused and the Escape is triggered
+        if (context.started && canPause)
+        {
+            PauseGame(); // Pause the game
+        }
+
+        // If paused and the Escape is triggered
+        else if (context.started && isPaused)
+        {
+            ResumeGame(); // Resume the game
+        }
+    }
+
     void PauseGame()
     {
+        Debug.Log("Game is paused");
+
         isPaused = true;
         Time.timeScale = 0;
         canPause = false;
-        MoveMenuInView();
+
+        // Hide UI elements
+        ToggleMenu(true);
     }
+
+    // Call to Resume the game
     public void ResumeGame()
     {
         isPaused = false;
         Time.timeScale = 1;
         canPause = true;
-        MoveMenuOutOfView();
+
+        // Hide UI elements
+        ToggleMenu(false);
     }
-    void MoveMenuInView()
-    {
-        visibleMenuPosition = pauseMenu.transform.position;
-        playerObject.transform.position = pauseMenu.transform.position;
-        pauseMenu.SetActive(true);
-        buttons.SetActive(true);
-        images.SetActive(true);
-    }
-    void MoveMenuOutOfView()
-    {   
-        pauseMenu.SetActive(false);
-        buttons.SetActive(false);
-        images.SetActive(false);
-    }
+
+    // Call to Restart the game
     public void RestartGame()
     {
         isPaused = false;
         Time.timeScale = 1;
-        SceneManager.LoadScene("AltLevel");
+        SceneManager.LoadScene(currentScene);
     }
+
+    // Call to Quit the game
     public void QuitGame()
     {
         isPaused = false;
         Time.timeScale = 1;
         SceneManager.LoadScene("MainMenu");
+    }
+    
+    void OnDestroy()
+    {
+        playerControls.GameControls.Disable();
     }
 }
