04b93234b1bfc362c77be2f262ea75d63887ee62
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-03-11 17:35:56 -0400

    Reformatted Comments in PlayerRespawn.cs
    Overview
    - This fixes #50.
    
    In-depth Details
    - These comments were added by @Annadaliah in 7b1642d.
    - As explained in #50, they were all one-liners.
    - I just reformatted them to be multi-line for easier reading.

--- 00 Unity Proj/Bubble/Assets/Scripts/Player/PlayerRespawn.cs
+++ 00 Unity Proj/Bubble/Assets/Scripts/Player/PlayerRespawn.cs
@@ -6,19 +6,44 @@ using UnityEngine.InputSystem.Android;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    private GameObject firstPlaySpawn; // Reference to where the player intially starts off in the beginning of the game
-    private Rigidbody2D playerRb; // Reference to the Players body
-    private GameObject currentTarget; // Reference to the most immediate target that the Player has to reach which will act as save points
-    private GameObject nextTarget; // Reference to following Target point after and this serves to ensure that everything is stored and able to function properly
-    private GameObject nextSpawn; // Reference to the Spawn point that the player will be using for the Respawn (before the UpdatedRespawn)
-    private UnityEngine.Vector2 currentSpawn; // Reference to the current save point that the player will spawn at when they die/hit an obstacle
+    // Reference to where the player intially starts off
+    // in the beginning of the game
+    private GameObject firstPlaySpawn;
+
+    // Reference to the Players body
+    private Rigidbody2D playerRb;
+
+    // Reference to the most immediate target that the
+    // Player has to reach which will act as save points
+    private GameObject currentTarget;
+
+    // Reference to following Target point after and
+    // this serves to ensure that everything is stored
+    // and able to function properly
+    private GameObject nextTarget;
+
+    // Reference to the Spawn point that the player
+    // will be using for the Respawn (before the
+    // UpdatedRespawn)
+    private GameObject nextSpawn;
+
+    // Reference to the current save point that the
+    // player will spawn at when they die/hit an obstacle
+    private UnityEngine.Vector2 currentSpawn;
     public int playerLives;
     private GameOver instanceOfGameOver;
-    private void Awake() // Tried to make sure that things are staying the right way for the player when loading into the game
+
+    // Tried to make sure that things are staying the
+    // right way for the player when loading into the game
+    private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
     }
-    public void Start() // Wanted to make sure that the Start Point of the player is set first and stay that way & that the main camera stays with the player
+
+    // Wanted to make sure that the Start Point of the
+    // player is set first and stay that way & that
+    // the main camera stays with the player
+    public void Start()
     {
         firstPlaySpawn = GameObject.FindGameObjectWithTag("FirstStart");
 
@@ -26,12 +51,23 @@ public class PlayerRespawn : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Obstacle")) //This tag is associated with Clouds for right now but can be used for any obstacles in the future depending if the tags need to be more specific
+        // This tag is associated with Clouds for right now
+        // but can be used for any obstacles in the future
+        // depending if the tags need to be more specific
+        if (collision.CompareTag("Obstacle"))
         {
-            Die(); //Will clear the player object, spawn the player back at the last save point
+            // Will clear the player object, spawn the
+            // player back at the last save point
+            Die();
         }
-        if (collision.CompareTag("TargetReached")) //This is the tag for only when the targets are reached, basically serving as a save point for the player so that they don't have to hit anythign further than just the arrow keys
+
+        if (collision.CompareTag("TargetReached"))
         {
+
+            // This is the tag for only when the targets are
+            // reached, basically serving as a save point
+            // for the player so that they don't have to hit
+            // anything further than just the arrow keys
             UpdateCurrentRespawnLocation();
         }
     }
@@ -40,24 +76,49 @@ public class PlayerRespawn : MonoBehaviour
         playerLives--;
         if (playerLives > 0)
         {
-            StartCoroutine(RespawnCoroutine(0.5f)); //This is important for the amount }of time between the player dying and the spawning back on the screen
+            // This is important for the amount of time between
+            // the player dying and the spawning back on the screen
+            StartCoroutine(RespawnCoroutine(0.5f));
         }
     }
-    IEnumerator RespawnCoroutine(float duration) // further breaks down what happens during respawn
+
+    // Further breaks down what happens during respawn
+    IEnumerator RespawnCoroutine(float duration)
     {
         playerRb.simulated = false;
         playerRb.velocity = new UnityEngine.Vector2(0, 0);
         transform.localScale = new UnityEngine.Vector2(0, 0);
         yield return new WaitForSeconds(duration);
-        nextSpawn = GameObject.FindGameObjectWithTag("TargetReached").GetComponent<ChangeTarget>().nextTargetPoint; // Makes sure that there is a connection between the information stored within the Targets and shared to this script
-        transform.position = currentSpawn; // Moves the player to the proper point 
-        transform.localScale = new UnityEngine.Vector2(1, 1); // might need to be changed later as I think it might be changing the scale of the character after the first respawn of the player
+
+        // Makes sure that there is a connection between
+        // the information stored within the Targets and
+        // shared to this script
+        nextSpawn = GameObject.FindGameObjectWithTag("TargetReached").GetComponent<ChangeTarget>().nextTargetPoint;
+        
+        // Moves the player to the proper point
+        transform.position = currentSpawn;
+
+        // might need to be changed later as I think it
+        // might be changing the scale of the character
+        // after the first respawn of the player
+        transform.localScale = new UnityEngine.Vector2(1, 1);
         playerRb.simulated = true;
     }
     void UpdateCurrentRespawnLocation()
     {
-        currentSpawn = currentTarget.transform.position; // Updates the current spawn point to the previous target point
-        currentTarget = GameObject.FindGameObjectWithTag("TargetReached").GetComponent<ChangeTarget>().nextTargetPoint; // Updates the further target that is stored witin the ChangeTarget script and makes sure everything interconnects
-        nextTarget = GameObject.FindGameObjectWithTag("TargetReached").GetComponent<ChangeTarget>().followingTargetPoint; // Updates the targets across the game, just insuring that there is the capability of multiple save points/spawn points that function whereever and whenever needed
+        // Updates the current spawn point to
+        // the previous target point
+        currentSpawn = currentTarget.transform.position;
+
+        // Updates the further target that is
+        // stored witin the ChangeTarget script
+        // and makes sure everything interconnects
+        currentTarget = GameObject.FindGameObjectWithTag("TargetReached").GetComponent<ChangeTarget>().nextTargetPoint;
+
+        // Updates the targets across the game, just
+        // insuring that there is the capability of
+        // multiple save points/spawn points that
+        // function whereever and whenever needed
+        nextTarget = GameObject.FindGameObjectWithTag("TargetReached").GetComponent<ChangeTarget>().followingTargetPoint;
     }
 }
