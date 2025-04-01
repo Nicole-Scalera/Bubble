97ccbcc62e805c9dc1d441357674bdbb08841928
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-04-01 15:59:17 -0400

    Setup Inheritance Framework for Vehicles
    Overview
    - Created Inheritance.unity scene
         - This is a copy of the Car Constructors scene (9c2950e)
    - Created 3 new scripts:
         - Vehicle.cs
         - Sedan.cs
         - Truck.cs
    
    In-depth Details
    - I will break down the relevant details below.
    
    A. New Scripts
    - No actual values, attributes, or methods are implemented in any of the new script files yet.
    - This commit is simply to upload the basic framework of an inheritance system in C# using the same car assets from 9c2950e.
    - This time, there are only two vehicles in the scene:
         - Green Sedan (Sedan.cs)
         - Red Truck (Truck.cs)
    - These inherit from Vehicle.cs, which inherits from Monobehaviour.
    
    B. Goals
    - More code will be added to these later on if I need to test out inheritance again.
    - However, upon further research, I have realized that a dependency injection (DI) pattern might be what I'm looking for when figuring out how to organize the Player and Target files for the overall project.
    - Fortunately, my work from 9c2950e should feed directly into this, since constructors are one way to perform DI.

---
+++ 00 Unity Proj/Bubble/Assets/Scenes/OOP_Test/Inheritance/Vehicle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Vehicle : MonoBehaviour
+{
+
+    // Attributes from Vehicle are inherited
+    // by child classes Truck and Sedan
+
+}
