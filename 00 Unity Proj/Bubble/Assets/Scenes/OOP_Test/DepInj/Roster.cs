e7399c7d20a54904c20e6ef84d53b96766af22ce
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-04-02 18:04:28 -0400

    Created Dependency Injection Experiment Framework
    Overview
    - Copied and edited a series of scripts from an article on DotNetTutorials.net.
         - Link: https://dotnettutorials.net/lesson/dependency-injection-design-pattern-csharp/
    - Added 4 new scripts to new DepInj folder in OOP_Test.
    
    In-depth Details
    - As mentioned in #56 and #57, I am trying my hand at the Dependency Injection (DI) pattern for solving spaghetti code issues.
         - Issues first raised back in #27
    - I copied these files from the DotNetTutorials article, which are made for a C# console app.
         - i.e. includes a Program.cs file as the main component.
    - However, because it's still C# & .NET, I can tinker them to work with Unity.
    - Likewise, this commit contains the general framework of DI, as described by this article.
    - Read below for specific details and changes.
    
    A. Scripts Added
    - The article provides the following scripts:
         - Program.cs
              - "Roster.cs," in this context.
         - Employee.cs
              - "Actor.cs," in this context.
         - EmployeeDAL.cs
              - "ActorDAL.cs," in this context.
         - EmployeeBL.cs
              - "ActorBL.cs," in this context.
    
    B. Roles of Each Script
    - The names of the scripts may change over time to suit the needs of the experiment.
         - The code itself will definitely change. This just provides a standard framework.
    - Here is a breakdown of what each Employee script does, as provided by DotNetTutorials.
    
    1. Employee.cs
         - "Model class" that contains the framework for Employee data.
         - Utilizedby EmployeeDAL.cs and EmployeeBL.cs.
    
    2. EmployeeDAL.cs
         - Interacts with the database in Employee.cs
         - Provides the methods to perform database operations.
    
    3. EmployeeBL.cs
         - The class where you implement the business logic and validation logic.
         - EmployeeBL will utilize methods from EmployeeDAL to save the data in the database.
    
    - Major Takeaways:
         - EmployeeBL.cs depends on EmployeeDAL.cs.
         - i.e. EmployeeDAL is the DEPENDENCY object; EmployeeBL is the DEPENDENT object.
    
    C. My Changes
    - If you haven't skimmed over the article, you might be wondering, "where is 'employee' coming from?"
    - As mentioned earlier, the author just used employee data in a database as an analogy to demonstrate DI.
    - Likewise, I have kept all the logic, but replaced the words "Employee" with "Actor"
    - The Program.cs file has been renamed to "Roster," as this is where the data regarding each actor in the scene will be printed.
    - Collectively, all of these have been temporarily added to a namespace called "DependencyInjection"
         - This is so that I can test them out in Unity without naming conflicts.
    
    D. Goals
    - Identify the workings of this framework, play around by adding debug statements in different areas of the project, etc.
    - Refactor code to fit the workings of the Target and Player scripts.
    - Document learning process!

---
+++ 00 Unity Proj/Bubble/Assets/Scenes/OOP_Test/DepInj/Roster.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DependencyInjection
+{
+    // This class will act as the Program file that will initiate everything
+    public class Roster : MonoBehaviour
+    {
+        static void Main(string[] args)
+        {
+            //Create an Instance of ActorBL and Inject the Dependency Object as an Argument to the Constructor
+            ActorBL actorBL = new ActorBL(new ActorDAL());
+            List<Actor> ListActor = actorBL.GetAllActors();
+            foreach (Actor act in ListActor)
+            {
+                Console.WriteLine($"ID = {act.ID}, Name = {act.Name}, Department = {act.Department}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
