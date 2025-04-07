5ebb1bb605d76329b71bcb46785047f405bd0e85
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-04-07 14:44:32 -0400

    Created GameManager for Dependency Injection Experiment
    Overview
    - Added GameManager.cs.
    - Added Temp.cs, copying code from the YouTube video I mentioned in 7a1ad54.
    
    In-depth Details
    - GameManager.cs does not have a lot of code right now, it's just set up to execute the actual dependency injection (DI) experiment.
    - Unity doesn't use standard Main(string[] args) method, everything is initialized via Game Objects and the scripts attached to them.
    - I might have to use a button to trigger a method, like I suggested for my inheritance experiment (#60).
    - I might have to pivot from DI for now, since it's not super heavily used, but will come back at a later time as needed.

--- 00 Unity Proj/Bubble/Assets/Scenes/OOP_Test/DepInj/Property/Player.cs
+++ 00 Unity Proj/Bubble/Assets/Scenes/OOP_Test/DepInj/Property/Player.cs
@@ -1,32 +1,27 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// namespace DependencyInjectionProperty
-// {
-//     //Dependency Object should be Interface-Based
-//     public interface IActorDAL
-//     {
-//         List<Actor> SelectAllActors();
+namespace DependencyInjectionProperty
+{
+    //Dependency Object should be Interface-Based
+    public interface IPlayerInfo
+    {
+        // void PlayerMovement();
+        // void Position();
+        // void Speed();
+        // void PlayerControls();
+        // void PlayerRB();
+    }
 
-//     }
+    //This is the class that is responsible for Interacting with the Database
+    //This class is going to be used by the ActorBL class
+    //That means it is going to be the Dependency Object
+    public class Player : IPlayerInfo
+    {
 
-//     //This is the class that is responsible for Interacting with the Database
-//     //This class is going to be used by the ActorBL class
-//     //That means it is going to be the Dependency Object
-//     public class Player
-//     {
-//         public List<Actor> SelectAllActors()
-//         {
-//             List<Actor> ListActors = new List<Actor>
-//             {
-//                 //Get the Actors from the Database
-//                 //for now we are hard coded the actors
-//                 new Actor() { ID = 1, Name = "Pranaya", Department = "IT" },
-//                 new Actor() { ID = 2, Name = "Kumar", Department = "HR" },
-//                 new Actor() { ID = 3, Name = "Rout", Department = "Payroll" }
-//             };
-//             return ListActor;
-//         }
-//     }
-// }
+        // private Position _position;
+        // private Saw _saw;
+
+    }
+}
