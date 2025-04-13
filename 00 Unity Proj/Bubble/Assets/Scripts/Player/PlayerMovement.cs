Nicole-Scalera/Bubble

commit 29abd2202b2862aa7e38d71b25f1262d6a9b728b

Implemented PersistentMonoSingleton for Player.cs
Overview
- This commit addresses #65.
- Started using UnityCommunity Singleton Package (263608f).

In-depth Details
- This is a great WIP update, I will break it up down below.

A. Rebuilt Singleton
- Added the UnityCommunity.UnitySingleton Package to the top of the Player.cs file.
- The class headline has been updated:
     - Previous: "public class Player : MonoBehaviour"
     - New: "public class Player : PersistentMonoSingleton<Player>"
- This allows Player.cs to use the PersistentMonoSingleton.cs file.
     - Purpose: creates a global, consistent singleton across scenes.
- I may have to switch my singleton method in the future, but this works for now.

B. Removed "Character" Instance
- The previous Singleton pattern in Player.cs was named "Character" (dfd8d26)
     - This is because PlayerInfo was already using "Instance".
- With the addition of PersistentMonoSingleton, I have removed it.

C. Updating References
- Updated the references to Player.Character in the following scripts:
     - PlayerMovement.cs
     - GameManager.cs

D. Reflection
- Just like I expressed in 59a883c, there are errors in these scripts that I will need to update.
- However, I'm on the right track by eliminating unnecessary calls in my program.
     - I talk about this more in #62.

00 Unity Proj/Bubble/Assets/Scripts/Player/PlayerMovement.cs
@@ -23,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         // ===== Player =====
-        player = Player.Character; // Access Player.cs
+        player = Player.Instance; // Access Player.cs
         GetPlayerInfo(); // Get the Player's info
         playerControls = Player.Controls; // Access movement controls
 
@@ -33,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
         GetTargetInfo(); // Get the Target's info
     }
 
-    public void GetPlayerInfo()
+    private void GetPlayerInfo()
     {
         // Get the following information about the Player
         playerRB = player.GetRigidbody2D(); // RigidBody2D
@@ -47,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log($"PlayerMovement.cs > GetPlayerInfo(): Player's vertical speed is {floatSpeed}");
     }
 
-    public void GetTargetInfo()
+    private void GetTargetInfo()
     {
         // Get the following information about the Target
         targetPos = targetInfo.GetTargetPosition(); // Target's Position

00 Unity Proj/Bubble/Assets/Scenes/Features/Obstacles_2.unity
@@ -1158,7 +1158,7 @@ GameObject:
   m_Component:
   - component: {fileID: 1165274843}
   - component: {fileID: 1165274848}
-  - component: {fileID: 1165274847}
+  - component: {fileID: 1165274850}
   - component: {fileID: 1165274846}
   - component: {fileID: 1165274845}
   - component: {fileID: 1165274844}
@@ -1260,21 +1260,6 @@ MonoBehaviour:
   m_Name: 
   m_EditorClassIdentifier: 
   targetInfo: {fileID: 0}
-  playerInfo: {fileID: 0}
---- !u!114 &1165274847
-MonoBehaviour:
-  m_ObjectHideFlags: 0
-  m_CorrespondingSourceObject: {fileID: 0}
-  m_PrefabInstance: {fileID: 0}
-  m_PrefabAsset: {fileID: 0}
-  m_GameObject: {fileID: 1165274842}
-  m_Enabled: 1
-  m_EditorHideFlags: 0
-  m_Script: {fileID: 11500000, guid: 8b44400fb1d7bcf4392bf48be541562d, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  moveSpeed: 5
-  floatSpeed: 5
 --- !u!212 &1165274848
 SpriteRenderer:
   m_ObjectHideFlags: 0
@@ -1340,6 +1325,21 @@ MonoBehaviour:
   m_Name: 
   m_EditorClassIdentifier: 
   playerLives: 0
+--- !u!114 &1165274850
+MonoBehaviour:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1165274842}
+  m_Enabled: 1
+  m_EditorHideFlags: 0
+  m_Script: {fileID: 11500000, guid: d7b37e4356a6b93488950d4e11917a93, type: 3}
+  m_Name: 
+  m_EditorClassIdentifier: 
+  UnparentOnAwake: 1
+  moveSpeed: 0
+  floatSpeed: 0
 --- !u!1 &1182851605
 GameObject:
   m_ObjectHideFlags: 0

00 Unity Proj/Bubble/Assets/Scripts/Game/GameManager.cs
@@ -37,7 +37,7 @@ public class GameManager : MonoBehaviour
         }
 
         // ===== Player =====
-        Player player = Player.Character; // Access Player.cs
+        Player player = Player.Instance; // Access Player.cs
 
         // ===== Target =====
         targetObject = GameObject.Find("Target"); // Assign Target GameObject

00 Unity Proj/Bubble/Assets/Scripts/Player/Player.cs
@@ -1,30 +1,12 @@
 using System;
 using UnityEngine;
+using UnityCommunity.UnitySingleton;
 
 // This is the PlayerInfo class. It encapsulates general data and info about
 // the Player game object, which is utilized throughout other scripts.
 
-public class Player : MonoBehaviour
+public class Player : PersistentMonoSingleton<Player>
 {
-    // Singleton instance for global reference
-    private static Player _player;
-
-    // Constructor that forces only a single
-    // instance of Player to be created
-    public static Player Character
-    {
-        get
-        {
-            // If Player instance is null, assign Player component
-            if (_player == null)
-            {
-                _player = GameObject.Find("Player").GetComponent<Player>();
-            }
-
-             // Return the Player instance
-            return _player;
-        }
-    }
     
     // Singleton instance for global reference
     private static PlayerControls _controls;

00 Unity Proj/Bubble/Assets/Scripts/Player/PlayerInfo.cs
@@ -19,7 +19,7 @@ public class PlayerInfo : MonoBehaviour
             // If Player instance is null, assign Player component
             if (_playerInfo == null)
             {
-                Player player = Player.Character; // Access Player.cs
+                Player player = Player.Instance; // Access Player.cs
                 _playerInfo = player.GetComponent<PlayerInfo>();
             }
 

00 Unity Proj/Bubble/ProjectSettings/EditorBuildSettings.asset
@@ -17,7 +17,7 @@ EditorBuildSettings:
   - enabled: 1
     path: Assets/Scenes/Features/Movement.unity
     guid: 8c9cfa26abfee488c85f1582747f6a02
-  - enabled: 1
+  - enabled: 0
     path: Assets/Scenes/Scene2.unity
     guid: 1e21d9dcebef3da44879377619bc43c4
   m_configObjects:

commit 8b552b17b0deac1a8419fe516dae4f8bea4e3413

Moved Get[Player/Target]Info(); to Start() in PlayerMovement.cs
Overview
- Updated PlayerMovement.cs.

In-depth Details
- Moved the GetPlayerInfo() and GetTargetInfo() methods into the Start() method of PlayerMovement.cs.
- The Player is still not moving on the screen, but is no longer throwing a NullReferenceException.
     - Error was caused by implementing PersistentMonoSingleton in 29abd22.
- I believe PlayerMovement.cs might be intialized before Player.cs.
- In that case, if it's calling for methods within Player.cs that are not fully initialized, it's trying to access something that doesn't exist.

00 Unity Proj/Bubble/Assets/Scripts/Player/PlayerMovement.cs
@@ -24,12 +24,22 @@ public class PlayerMovement : MonoBehaviour
     {
         // ===== Player =====
         player = Player.Instance; // Access Player.cs
-        GetPlayerInfo(); // Get the Player's info
         playerControls = Player.Controls; // Access movement controls
 
         // ===== Target =====
-        target = Target.Instance; // Access Player.cs
+        target = Target.Instance; // Access Target.cs
+    }
+    
+    void Start()
+    {
+        GetPlayerInfo(); // Get the Player's info
         GetTargetInfo(); // Get the Target's info
+        
+        // When the script is called, grab the player's starting
+        // position and set it as the current position.
+        playerPos = startPos;
+        // The player's position will then
+        // continuously be updated in Update()
     }
 
     private void GetPlayerInfo()
@@ -55,14 +65,7 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log($"PlayerMovement.cs > GetTargetInfo(): Target's position is {targetPos}");
     }
 
-    void Start()
-    {
-        // When the script is called, grab the player's starting
-        // position and set it as the current position.
-        playerPos = startPos;
-        // The player's position will then
-        // continuously be updated in Update()
-    }
+
 
     private void OnEnable()
     {